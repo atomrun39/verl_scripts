f13f613476ed41d8a0e3a68cb23e6cfba0bbdedc
chenyd13 <chenyd13@chinatelecom.cn>
2026-02-05 15:02:40 +0800

    update script

--- cyd_run_qwen3_4b_grpo_npu.sh
+++ cyd_run_qwen3_4b_grpo_npu.sh
@@ -5,7 +5,7 @@ source /usr/local/Ascend/nnal/atb/set_env.sh
 # 使用v1引擎
 export VLLM_USE_V1=1
 # 指定vllm 版本
-export VLLM_VERSION=0.9.1
+# export VLLM_VERSION=0.9.1
 
 # 开启二级流水
 export TASK_QUEUE_ENABLE=2
@@ -35,14 +35,19 @@ mkdir -p "${RAY_DATA_HOME}/logs/${trainer_project_name}"
 LOG_PATH="${RAY_DATA_HOME}/logs/${trainer_project_name}/${trainer_experiment_name}.log"
 
 use_dynamic_bsz=True
+max_prompt_length=4096
+max_response_length=16384
+max_tokens=$(max_prompt_length  + max_response_length)
+log_prob_max_tokens=$(max_tokens * 2)
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=${TRAIN_FILE} \
     data.val_files=${TEST_FILE} \
     data.train_batch_size=512 \
-    data.max_prompt_length=2048 \
-    data.max_response_length=8192 \
+    data.max_prompt_length=${max_prompt_length} \
+    data.max_response_length=${max_response_length} \
+    data.prompt_key=prompt \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path=${MODEL_PATH} \
@@ -55,20 +60,20 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.use_torch_compile=False \
     actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=3000 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${max_tokens} \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.enforce_eager=True \
     actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
-    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=4096 \
+    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${log_prob_max_tokens} \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.9 \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.ref.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
-    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=8192 \
+    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=${log_prob_max_tokens} \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.ref.use_torch_compile=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
@@ -79,7 +84,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.default_local_dir=${CKPTS_DIR} \
     trainer.n_gpus_per_node=$trainer_n_gpus_per_node \
     trainer.nnodes=$trainer_nnodes \
-    trainer.save_freq=-1 \
+    trainer.save_freq=10 \
     trainer.test_freq=10 \
     trainer.total_epochs=15 \
     trainer.val_before_train=False 2>&1 | tee ${LOG_PATH}
