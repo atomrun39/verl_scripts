91bc2d55929cc1d3731e3bc2ee3ce0aa931cf638
chenyd13 <chenyd13@chinatelecom.cn>
2026-02-09 16:39:30 +0800

    updata grpo

--- cyd_run_qwen3_4b_grpo_npu.sh
+++ cyd_run_qwen3_4b_grpo_npu.sh
@@ -1,7 +1,12 @@
 set -xeuo pipefail
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
-source /usr/local/Ascend/nnal/atb/set_env.sh
-
+#source /usr/local/Ascend/ascend-toolkit/set_env.sh
+#source /usr/local/Ascend/nnal/atb/set_env.sh
+export HCCL_EXEC_TIMEOUT=18000
+export HCCL_CONNECT_TIMEOUT=1800
+export GLOO_SOCKET_IFNAME=eno0
+export TP_SOCKET_IFNAME=eno0
+export HCCL_SOCKET_IFNAME=eno0
+export PYTORCH_NPU_ALLOC_CONF="expandable_segments:False,max_split_size_mb:128,garbage_collection_threshold:0.6"
 # 使用v1引擎
 export VLLM_USE_V1=1
 # 指定vllm 版本
@@ -18,15 +23,15 @@ export TIMESTAMP=$(date +"%m-%d_%H-%M")
 
 # 机器单机8卡
 trainer_n_gpus_per_node=8
-trainer_nnodes=1
+trainer_nnodes=2
 trainer_project_name='qwen3-grpo'
-trainer_experiment_name='qwen3_4b_grpo_1node'-${TIMESTAMP}
+trainer_experiment_name='qwen3_4b_grpo_2node'-${TIMESTAMP}
 
-RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}/verl"}
-MODEL_PATH=${MODEL_PATH:-"${RAY_DATA_HOME}/models/Qwen3-4B"}
+RAY_DATA_HOME=${RAY_DATA_HOME:-"/data/cyd/verl"}
+MODEL_PATH=${MODEL_PATH:-"/data/cyd/weights/Qwen3-4B"}
 CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${trainer_project_name}/${trainer_experiment_name}"}
-TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/data/dapo-math-17k_dedup_r1_sys_prompt_mathdapo/train.parquet"}
-TEST_FILE=${TEST_FILE:-"${RAY_DATA_HOME}/data/dapo-math-17k_dedup_r1_sys_prompt_mathdapo/test.parquet"}
+TRAIN_FILE=${TRAIN_FILE:-"/data/cyd/dapo-math-17k/train.parquet"}
+TEST_FILE=${TEST_FILE:-"/data/cyd/dapo-math-17k/test.parquet"}
 
 mkdir -p ./swanlab_logs/${trainer_project_name}
 export SWANLAB_LOG_DIR=./swanlab_logs/${trainer_project_name}/${trainer_experiment_name}
@@ -35,26 +40,28 @@ mkdir -p "${RAY_DATA_HOME}/logs/${trainer_project_name}"
 LOG_PATH="${RAY_DATA_HOME}/logs/${trainer_project_name}/${trainer_experiment_name}.log"
 
 use_dynamic_bsz=True
-max_prompt_length=4096
-max_response_length=16384
-max_tokens=$(max_prompt_length  + max_response_length)
-log_prob_max_tokens=$(max_tokens * 2)
+max_prompt_length=2048
+max_response_length=8192
+max_tokens=$((max_prompt_length  + max_response_length))
+log_prob_max_tokens=$((max_tokens * 2))
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=${TRAIN_FILE} \
     data.val_files=${TEST_FILE} \
-    data.train_batch_size=512 \
+    data.train_batch_size=32 \
     data.max_prompt_length=${max_prompt_length} \
     data.max_response_length=${max_response_length} \
     data.prompt_key=prompt \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
+    actor_rollout_ref.actor.strategy=fsdp \
     actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.entropy_coeff=0.001 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -67,6 +74,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.enforce_eager=True \
     actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${log_prob_max_tokens} \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
@@ -74,13 +82,14 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.ref.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
     actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=${log_prob_max_tokens} \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.ref.use_torch_compile=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.project_name=${trainer_project_name} \
     trainer.experiment_name=${trainer_experiment_name} \
-    trainer.logger=['console','swablab'] \
+    trainer.logger=['console','swanlab'] \
     trainer.default_local_dir=${CKPTS_DIR} \
     trainer.n_gpus_per_node=$trainer_n_gpus_per_node \
     trainer.nnodes=$trainer_nnodes \
